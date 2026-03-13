e2f6ec4e41b766a79394b2b44e6c121833a01362
Abhinav Singh <SinghA@alphavima.com>
2026-03-13 16:40:25 +0530

    folder structure setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Deployment Tracking</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Clientboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Clientboard() {
+  return (
+    <div>Clientboard</div>
+  )
+}
+
+export default Clientboard;
